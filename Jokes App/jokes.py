f21ddc7c60856bc55e5d940b022124006b4a077a
Garo-U <adityadurugkar07@gmail.com>
2023-01-05 00:20:24 +0530

    Python Streamlit WebApp that Tells Jokes/Puns

---
+++ Jokes App/jokes.py
@@ -0,0 +1,25 @@
+import requests
+import pyttsx3
+import speech_recognition as sr
+import streamlit as st
+import sys
+
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+engine = pyttsx3.init()
+engine.setProperty('rate', 175)
+
+def text_to_speech(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+if st.button("Tell me a joke"):  
+    response = requests.get('https://icanhazdadjoke.com', headers={'Accept':'application/json'})
+    joke = response.json()['joke']
+    st.write(joke)  
+    text_to_speech(joke)  
+
+    
+
