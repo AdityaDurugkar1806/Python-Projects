0f6bc3d93042d30088f27d59a631a78410f637a2
Garo-U <adityadurugkar07@gmail.com>
2023-01-08 11:16:08 +0530

    Just Basic

--- Jokes App/jokes.py
+++ Jokes App/jokes.py
@@ -3,23 +3,25 @@ import pyttsx3
 import speech_recognition as sr
 import streamlit as st
 import sys
+def main():
+    r = sr.Recognizer()
+    mic = sr.Microphone()
 
-r = sr.Recognizer()
-mic = sr.Microphone()
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 175)
 
-engine = pyttsx3.init()
-engine.setProperty('rate', 175)
-
-def text_to_speech(text):
-    engine.say(text)
-    engine.runAndWait()
+    def text_to_speech(text):
+        engine.say(text)
+        engine.runAndWait()
 
 
-if st.button("Tell me a joke"):  
-    response = requests.get('https://icanhazdadjoke.com', headers={'Accept':'application/json'})
-    joke = response.json()['joke']
-    st.write(joke)  
-    text_to_speech(joke)  
+    if st.button("Tell me a joke"):  
+        response = requests.get('https://icanhazdadjoke.com', headers={'Accept':'application/json'})
+        joke = response.json()['joke']
+        st.write(joke)  
+        text_to_speech(joke)  
 
     
+if __name__ == "__main__":
+    main()
 
